bdcae832e7c516a052f2d00dd61fe1d61a634d74
경이현 <wm0755123@naver.com>
2025-10-16 18:15:10 +0900

    feat: 도메인 네임 CORS 허용

--- backend/src/main/java/com/my_board/global/config/WebConfig.java
+++ backend/src/main/java/com/my_board/global/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://devboard.kr")
                         .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
                         .allowCredentials(true);
             }
